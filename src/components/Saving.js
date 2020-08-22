5e70e1b6b453281bc1d8e296c07cbaf3fa196a73
HtetMonOo <htetmonoo43@gmail.com>
2020-08-22 19:33:03 +0630

    Saving design confirm

--- src/components/Saving.js
+++ src/components/Saving.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import './Saving.css';
+import { buildUrl } from '../utils';
 
 const Saving = ({departments}) => {
     
     return (
-        <div className="Saving m-2 rounded-sm">
-            <h5 className="text-left p-2 m-0">Saving Spotlight</h5>
+        <div className="Saving m-2">
+            <div className="d-flex justify-content-between"> 
+                <h4 className="text-left p-2 m-0 font-weight-bold">Saving Spotlight <i class="fas fa-comment-dollar"></i></h4>
+                <h5 className="text-right p-2"><i class="fas fa-angle-double-left"></i> &nbsp; <i class="fas fa-angle-double-right"></i></h5>
+            </div>
+            
             <div className="holder">
                 {
                     departments.map(dept => (
                         dept.name === "Savings spotlight" &&
                         dept.departments.map(dep => (
-                            dep.department.title === "Savings spotlight" &&
-                            dep.categories.map(cat => (
-                                <div className="card m-1 p-2" key={cat.category.title}>{cat.category.title}</div>
-                            ))
+                            dep.department.title !== "Savings spotlight" &&
+                                <a className="card m-1 p-2" key={dep.department.title} href={dep.department.clickThrough.rawValue? buildUrl(dep.department.clickThrough.rawValue) : "#"}>{dep.department.title}</a>
+                            
                         ))
                     ))
                 }
